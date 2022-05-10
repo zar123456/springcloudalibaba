ea6b33fcd62bdb38e41f958f9ac9f0bceeb1b8ef
zar123456 <475682857@qq.com>
2022-05-10 17:40:11 +0800

    添加提供者

--- nacos/src/main/java/com/alibab/noacs/discovery/DiscoveryApplication.java
+++ nacos/src/main/java/com/alibab/noacs/discovery/DiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.alibab.noacs.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
