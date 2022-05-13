bb6378dcb9d0b47ca851acde64a6a99db25f23e4
zar123456 <475682857@qq.com>
2022-05-13 10:48:47 +0800

    修改配置文件

--- nacos-config/src/main/java/com/alibab/noacs/config/NacosConfigApplication.java
+++ nacos-config/src/main/java/com/alibab/noacs/config/NacosConfigApplication.java
@@ -1,18 +1,13 @@
 package com.alibab.noacs.config;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NacosConfigApplication {
 
     public static void main(String[] args) {
-      //  SpringApplication.run(NacosConfigApplication.class, args);
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfigApplication.class, args);
-        String userName = applicationContext.getEnvironment().getProperty("user.name");
-        String userAge = applicationContext.getEnvironment().getProperty("user.age");
-        System.err.println("user name :" +userName+"; age: "+userAge);
+       SpringApplication.run(NacosConfigApplication.class, args);
     }
 }
--- nacos-config/src/main/java/com/alibab/noacs/config/controller/ConfigController.java
+++ nacos-config/src/main/java/com/alibab/noacs/config/controller/ConfigController.java
@@ -1,7 +1,10 @@
 package com.alibab.noacs.config.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +16,23 @@ public class ConfigController {
     @Value("${useLocalCache:false}")
     private boolean useLocalCache;
 
+    @Value("${springboot.user.name}")
+    private String name;
+
+    @Value("${springboot.user.age}")
+    private String age;
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
     /**
      * http://localhost:8080/config/get
      */
     @RequestMapping("/get")
     public boolean get() {
         return useLocalCache;
+    }
+
+    @RequestMapping("/getUser")
+    public String getConf() {
+        return "name = " + name + "; age = " + age;
     }
 }
