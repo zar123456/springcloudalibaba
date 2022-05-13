7901a5541dd46330e5da733fc9410481e1680ef2
zar123456 <475682857@qq.com>
2022-05-13 13:51:03 +0800

    去掉target

--- nacos-cousumer/src/main/java/com/alibab/nacos/cousumer/controller/EchoController.java
+++ nacos-cousumer/src/main/java/com/alibab/nacos/cousumer/controller/EchoController.java
@@ -1,13 +1,18 @@
 package com.alibab.nacos.cousumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 public class EchoController {
 
     @Autowired
@@ -15,6 +20,11 @@ public class EchoController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+//    @Autowired
+//    public EchoController(RestTemplate restTemplate){
+//        this.restTemplate = restTemplate;
+//    }
 
     @GetMapping("/echo/app-name/{str}")
     public String echoAppName(@PathVariable String str){
